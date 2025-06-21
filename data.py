8bd6b86f94d28fef29ae6c112718ae525a529eb6
Sherif Gamal <sherifgamal9441@gmail.com>
2025-06-21 14:04:09 +0300

    updated data.py and added .dvc data

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
+import numpy as np
 
 def load_mnist():
     "Loads training data and preprocess it"
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    data = np.load("mnist_data.npz")
+    x_test, y_train , x_test, y_test = data["x_train"], data["y_train"], data["x_test"], data["y_test"]
     x_train = x_train.astype('float32') / 255.0
     x_test = x_test.astype('float32') / 255.0
     return (x_train, y_train), (x_test, y_test)
